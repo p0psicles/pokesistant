d58fdf8534f00f13f50e805922ff0dbb94c95813
P0psicles <rogier@headshots.nl>
2016-08-21 14:56:19 +0200

    Added the controller for displaying the move details.

--- server/public/static/js/pokemonList.js
+++ server/public/static/js/pokemonList.js
@@ -12,7 +12,7 @@ define([
 
     return angular.module('myApp.pokemonList', ['ngRoute'])
 
-        .controller('PokemonListCtrl', ['$scope', '$http', 'PokemonService', function ($scope, $http, PokemonService) {
+        .controller('PokemonListCtrl', ['$scope', '$http', 'PokemonService', 'dialogs', function ($scope, $http, PokemonService, dialogs) {
             $scope.pokemonList = PokemonService.getPokemons();
             $scope.updateList = function () {
                 $http.get('/pokemon').then(function (response) {
@@ -20,5 +20,18 @@ define([
                     $scope.pokemonList = PokemonService.getPokemons();
                 });
             };
-        }]);
+            $scope.openMoveDetails = function (moveId) {
+                dialogs.create('static/partials/moveDetails.html', 'moveDetailsController', moveId, {}, 'ctrl');
+            };
+        }])
+        .controller('moveDetailsController', function ($scope, $uibModalInstance, $http, $filter, data) {
+            $scope.data = data;
+            $http.get('/static/json/GAME_DATA_MOVES.json').then(function (response) {
+                $scope.moveDetails = $filter('filter')(response.data, {ID: $scope.data}, true)[0];
+                $scope.header = $scope.moveDetails.Name;
+            });
+            $scope.done = function () {
+                $uibModalInstance.close($scope.data);
+            };
+        });
 });
